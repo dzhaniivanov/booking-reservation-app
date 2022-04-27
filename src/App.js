346fad2247cfb5ebb4b6997cbcecbb952d77eeb5
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-04-27 13:44:31 +0300

    setup layout

--- src/App.js
+++ src/App.js
@@ -1,8 +1,17 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import List from "./pages/List/List";
+import Home from "./pages/Home/Home";
+import Hotel from "./pages/Hotel/Hotel";
+
 function App() {
   return (
-    <div>
-      Hello world!
-    </div>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/hotels" element={<List />} />
+        <Route path="/hotels/:id" element={<Hotel />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
