bac01564d18d7cb14b77939a68539fa252e79d22
dzhaniivanov94 <68897146+dzhaniivanov94@users.noreply.github.com>
2022-04-28 11:54:46 +0300

    featured properties section added

--- src/pages/Home/Home.js
+++ src/pages/Home/Home.js
@@ -1,6 +1,7 @@
 import Featured from "../../components/Featured/Featured";
 import FeaturedProperties from "../../components/FeaturedProperties/FeaturedProperties";
 import Header from "../../components/Header/Header";
+import MailList from "../../components/MailList/MailList";
 import Navbar from "../../components/Navbar/Navbar";
 import PropertyList from "../../components/PropertyList/PropertyList";
 import "./Home.css";
@@ -15,7 +16,8 @@ const Home = () => {
         <h1 className="homeTitle">Browse by property type</h1>
         <PropertyList />
         <h1 className="homeTitle">Homes guests love</h1>
-        <FeaturedProperties/>
+        <FeaturedProperties />
+        <MailList />
       </div>
     </div>
   );
